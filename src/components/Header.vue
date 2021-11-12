154df3b413c9da2bd34b9e63d9b52c8dbcc52fe1
Sebastian <roshfeltsemo@gmail.com>
2021-11-12 17:34:10 -0500

    make navbar  responsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,13 @@
 <template>
-    <header class="flex justify-between items-center bg-white sticky top-0 border-b border-gray-300 py-4">
-        <div @click="toHomePage" class="cursor-pointer">
-            <h1 class="text-6xl lg:text-5xl sm:text-4xl">recipes</h1>
-            <span class="text-xl lg:text-base">the best recipe website</span> 
+    <header class="bg-white sticky top-0 border-b border-gray-300 py-4">
+        <div class="flex justify-between items-center mx-auto max-w-3xl lg:max-w-xl sm:max-w-xs xs:px-4">
+            <div @click="toHomePage" class="cursor-pointer">
+                <h1 class="text-6xl lg:text-5xl sm:text-4xl">recipes</h1>
+                <span class="text-xl lg:text-base">the best recipe website</span> 
+            </div>
+            
+            <button v-show="!isHidden" @click="toAddRecipe" class="bg-blue-600 text-white rounded p-4 hover:bg-blue-500 text-2xl lg:text-xl lg:p-3 sm:p-2 xs:text-base">add recipe</button>
         </div>
-        
-        <button v-show="!isHidden" @click="toAddRecipe" class="bg-blue-600 text-white rounded p-4 hover:bg-blue-500 text-2xl lg:text-xl lg:p-3 sm:p-2 xs:text-base">add recipe</button>
     </header>
 </template>
 
